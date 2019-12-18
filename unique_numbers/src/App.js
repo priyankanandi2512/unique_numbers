da97eb3f0988c35ad3b575957d68886402d27e4e
Priyanka Nandi <priyanka.nandi@infobip.com>
2019-12-18 12:36:37 +0530

    find duplicates and unique count from input value

--- unique_numbers/src/App.js
+++ unique_numbers/src/App.js
@@ -1,38 +1,57 @@
 import React from 'react';
 import './App.css';
-
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state ={
-            value: ''
+        this.state = {
+            inputValue: ''
         }
-        this.initialArray = [ 1,2,3,4,5];
+        this.existingArray = [1, 2, 3, 4, 5];
     }
 
     render() {
         return (
             <div className="App">
-                <input type="text" pattern="^[0-9]+[,0-9-]+"
-                    onChange={this.handleChange} 
-                    value={this.state.value}
+                <input type="text"
+                    onChange={this.handleChange}
+                    value={this.state.inputValue}
                 />
                 <button onClick={this.handleSubmit}>
-                    Submit 
+                    Submit
                 </button>
             </div>
         );
     }
 
     handleChange = (e) => {
-        this.setState({value: e.target.value})
+        this.setState({ inputValue: e.target.value })
     }
 
+    getNumbers = (acc, num) => {
+        if (num.split("-").length === 2) {
+            const range = num.split("-");
+            const rangeStart = parseInt(range[0]);
+            const rangeEnd = parseInt(range[1]);
+            const list = [];
+            for (var i = rangeStart; i <= rangeEnd; i++) {
+                list.push(i);
+            }
+            acc = acc.concat(list);
+        }
+        else if (!isNaN(parseInt(num))) {
+            const intVal = parseInt(num);
+            acc.push(intVal);
+        }
+        return acc;
+
+    }
     handleSubmit = (e) => {
-        const { value } = this.state;
-        const numbers = value.split(',');
-        const uniques = numbers.filter(this.onlyUnique);
-        console.log(uniques); 
+        const { inputValue } = this.state;
+        const numbers = inputValue.split(',').reduce(this.getNumbers, []);
+        const uniqueNumbers = numbers.filter(this.onlyUnique);
+        const duplicates = this.existingArray.filter(val => uniqueNumbers.includes(val));
+        console.log('duplicates from existing are:', duplicates);
+        console.log('unique array count is:', this.existingArray.length - duplicates.length);
     }
 
     onlyUnique = (value, index, self) => self.indexOf(value) === index;
