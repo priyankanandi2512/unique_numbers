ed94f94da88b1706c69c1a87f358114d048624bb
Priyanka Nandi <priyanka.nandi@infobip.com>
2019-12-18 13:26:53 +0530

    cleanup

--- unique_numbers/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import UniqueNumber from './unique-number';
-
-ReactDOM.render(<UniqueNumber />, document.getElementById('root'));
-
